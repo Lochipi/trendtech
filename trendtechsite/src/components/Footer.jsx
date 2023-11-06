7a1896d91ede17b0e601da99c0e549f3b72e0088
Cornelius Emase <corneliuslochipi@gmail.com>
2023-11-06 21:11:45 +0300

    updated footer ui

--- trendtechsite/src/components/Footer.jsx
+++ trendtechsite/src/components/Footer.jsx
@@ -1,30 +1,200 @@
 import { FooterStyled } from "./styles/Footer.styled";
 
 function Footer() {
+  const year = new Date().getFullYear();
   return (
     <FooterStyled>
-      <div className="footer-content">
-        <div className="footer-logo">
-          <h1>TrendTech Innovations</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing <br /> elit. Dolores,
-            soluta?
-          </p>
+      <div class="footer-line"></div>
+      <div class="footer-wrapper">
+        <section class="footer-top">
+          <div class="footer-headline">
+            <h2>Subscribe to our newsletter</h2>
+            <p>Stay up to date with our news and articles</p>
+          </div>
+          <div class="footer-subscribe">
+            <input type="email" spellcheck="false" placeholder="Your Email" />
+            <button>Sign Up</button>
+          </div>
+        </section>
+        <div class="footer-columns">
+          <section class="footer-logo">
+            <h2>
+              <svg
+                width="1103"
+                height="996"
+                viewBox="0 0 1103 996"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M410.988 255.56L0 995.337H189.802L505.141 427.427L410.988 255.56ZM1102.94 995.337L647.119 170.373L551.471 0L457.317 170.373L551.471 340.746L711.79 629.718H498.683L405.461 786.972H799.034L914.634 995.337H1102.94Z"
+                  fill="#FAFBFC"
+                />
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M410.988 255.56L0 995.337H189.802L505.141 427.427L410.988 255.56ZM1102.94 995.337L647.119 170.373L551.471 0L457.317 170.373L551.471 340.746L711.79 629.718H498.683L405.461 786.972H799.034L914.634 995.337H1102.94Z"
+                  fill="#FAFBFC"
+                />
+              </svg>
+            </h2>
+          </section>
+          <section>
+            <h3>Product</h3>
+            <ul>
+              <li>
+                <a href="#" title="API">
+                  API
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Pricing">
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Documentation">
+                  Documentation
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Release Notes">
+                  Release Notes
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Status">
+                  Status
+                </a>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h3>Resources</h3>
+            <ul>
+              <li>
+                <a href="#" title="Support">
+                  Support
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Sitemap">
+                  Sitemap
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Newsletter">
+                  Newsletter
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Help Centre">
+                  Help Centre
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Investor">
+                  Investor
+                </a>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h3>Company</h3>
+            <ul>
+              <li>
+                <a href="#" title="About Us">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Blog">
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Careers">
+                  Careers
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Press">
+                  Press
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h3>Legal</h3>
+            <ul>
+              <li>
+                <a href="#" title="Terms and services">
+                  Terms
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Privacy Policy">
+                  Privacy
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Cookies">
+                  Cookies
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Licenses">
+                  Licenses
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Cookies">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </section>
         </div>
-        <div className="footer-links">
-          <p>
-            <a href="#">Home</a>
-          </p>
-          <p>
-            <a href="#">About</a>
-          </p>
-          <p>
-            <a href="#">Shop With us</a>
-          </p>
+        <div class="footer-bottom">
+          <div class="social-links">
+            <ul>
+              <li>
+                <a href="#" title="Instagram">
+                  <img src="/footericons/instagram.svg" alt="Instagram" />
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Linkedin">
+                  <img src="/footericons/linkedin.svg" alt="Linkedin" />
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Twitter">
+                  <img src="/footericons/twitter.svg" alt="Twitter" />
+                </a>
+              </li>
+              <li>
+                <a href="#" title="Youtube">
+                  <img src="/footericons/youtube.svg" alt="YouTube" />
+                </a>
+              </li>
+              <li>
+                <a href="#" title="GitHub">
+                  <img src="/footericons/github.svg" alt="GitHub" />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <small>© Atheros Intelligence Ltd. {year}, All rights reserved</small>
         </div>
-      </div>
-      <div className="footer-copyright">
-        <p>All rights reserved @2023</p>
       </div>
     </FooterStyled>
   );
--- trendtechsite/src/components/styles/Footer.styled.jsx
+++ trendtechsite/src/components/styles/Footer.styled.jsx
@@ -1,74 +1,227 @@
 import styled from "styled-components";
 
-export const FooterStyled = styled.div`
-  min-height: 200px;
+export const FooterStyled = styled.footer`
+  background-color: #121A29;
+  min-width: 300px;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+
+.footer-wrapper {
+  display: flex;
+  background: var(--footer-background);
+  flex-direction: column;
+  max-width: 1320px;
+  padding: 16px;
+}
+
+
+/* top section */
+.footer-top {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: start;
+  align-items: start;
+  padding: 16px 0px;
+  justify-content: space-between;
+}
+
+.footer-line {
+  display: block;
   width: 100%;
-  background: linear-gradient(#3a6d70, gray);
+  height: 8px;
+}
+
+/* footer subscribe */
+.footer-subscribe {
+  display: flex;
+  gap: 12px;
+  flex-wrap: wrap;
+}
+
+.footer-subscribe > input {
+  border: 1px solid gray;
   color: whitesmoke;
+  outline: none;
+  min-height: 30px;
+  background-color: var(--footer-background);
+  font-size: 1.1rem;
+  flex: 1 0 120px;
+  padding: 8px 12px;
+  border-radius: 8px;
+}
 
-  .footer-content {
-    display: flex;
-    padding: 0.5em 1em;
-  }
+.footer-subscribe > input::placeholder {
+  color: gray;
+}
 
-  & .footer-logo {
-    flex: 1;
-  }
+.footer-subscribe > button {
+  cursor: pointer;
+  background-color: slate;
+  border-radius: 8px;
+  color: white;
+  font-size: 1.25rem;
+  min-width: 120px;
+  min-height: 36px;
+  flex: 1 0 80px;
+  white-space: nowrap;
+  padding: 8px 12px;
+  border: 0px;
+  outline: none;
+  border: 1px solid gray;
+}
 
-  & .footer-logo h1 {
-    font-size: 2rem;
-  }
+/* footer columns */
+.footer-columns {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: start;
+  align-items: start;
+  flex: 2 0 140px;
+  width: 100%;
+  gap: 16px;
+  padding: 24px 8px 16px 8px;
+  margin: 0 auto;
+}
 
-  & .footer-links {
-    flex: 2;
-  }
+.footer-columns ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
 
-  & .footer-links p:first-child {
-    margin-top: 2em;
-  }
+.footer-columns ul a {
+  color: var(--text-color);
+  text-decoration: none;
+}
 
-  & .footer-links p {
-    line-height: 1;
-  }
+.footer-columns ul a:hover{
+  text-decoration: underline;
+}
 
-  & .footer-links p a {
-    text-decoration: none;
-    color: whitesmoke;
-  }
+.footer-columns ul li {
+  margin-bottom: 16px;
+}
 
-  & .footer-links p a:hover {
-    text-decoration: underline;
-    color: rgba(173, 216, 230, 1);
-  }
+.footer-columns h3 {
+  color: var(--text-primary);
+  margin-top: 0;
+  margin-bottom: 25px;
+  font-size: 1.125rem;
+}
 
-  & .footer-copyright {
-    padding: 0 1em;
-  }
+.footer-centering {
+  margin: 0 auto;
+}
 
-  @media (max-width: 568px) {
-    & .footer-logo h1 {
-      font-size: 1.8rem;
-    }
+.footer-columns > section {
+  min-width: 150px;
+}
 
-    & .footer-logo p {
-      font-size: 0.8rem;
-    }
+.footer-logo {
+  display: flex;
+  flex-direction: row;
+  align-items: start;
+  justify-content: start;
+  gap: 16px;
+  color: var(--text-color);
+  margin-bottom: 60px;
+}
 
-    & .footer-links p {
-      line-height: 1;
-    }
+.footer-logo svg {
+  stroke: var(--text-color);
+  stroke-width: 8px;
+  width: 50px;
+  height: 50px;
+}
 
-    .footer-content {
+
+/* Footer bottom */
+
+.footer-bottom {
+  text-align: center;
+  margin-top: 48px;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  gap: 16px;
+  padding: 16px 0px;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  color: var(--text-gray);
+}
+
+.footer-bottom > small {
+  font-size: 16px;
+  margin: 0 4px;
+}
+
+.footer-headline > h2 {
+  margin: 0px;
+  color: var(--text-primary);
+}
+
+.footer-headline > p {
+  color: var(--text-gray);
+  margin: 4px 0px 20px 0px;
+}
+
+.footer-line {
+  display: block;
+  min-width: 40px;
+  height: 2px;
+  background-color: var(--text-gray);
+}
+
+/*? socials */
+
+.social-links {
+  display: flex;
+  gap: 8px;
+  align-items: center;
+}
+
+.social-text {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.social-links img {
+  width: 24px;
+  height: 24px;
+  transition: all 0.2s ease-in-out;
+}
+
+.social-links img:hover {
+  transform: scale(1.1);
+}
+
+.social-links ul {
+  display: flex;
+  gap: 12px;
+  list-style-type: none;
+}
+
+/* mobile */
+
+@media (max-width: 800px) {
+  .footer-top {
       display: flex;
-      gap: 1em;
-      padding: 0.5em 1em;
-    }
+      flex-direction: column;
+      align-items: center;
+      text-align: center;
+      padding: 16px 8px 32px 8px;
+  }
 
-    & .footer-logo {
-      flex: 1;
-    }
-    & .footer-links {
-      flex: 1;
-    }
+  .footer-bottom {
+      display: flex;
+      flex-direction: column-reverse;
+      align-items: space-between;
+      justify-content: center;
+      margin: 0 auto;
   }
 `;
